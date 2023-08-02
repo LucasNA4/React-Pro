7bf6b7cef25df0cb9faeaf10810a45db7e648921
LucasNA4 <104040964+LucasNA4@users.noreply.github.com>
2023-08-02 16:39:39 -0300

    Primer commit rama routerdom6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -3,6 +3,8 @@ import { Routes, Route, NavLink, Navigate } from 'react-router-dom'
 
 import logo from '../logo.svg'
 
+import { LazyPage1, LazyPage2, LazyPage3 } from "./01-lazyload/pages";
+
 
 export const Navigation = () => {
 
@@ -16,21 +18,21 @@ export const Navigation = () => {
                     
                     <ul>
                         <li>
-                            <NavLink to="/home" className={ ({ isActive }) => activeLink(isActive) }>Home</NavLink>
+                            <NavLink to="/lazy1" className={ ({ isActive }) => activeLink(isActive) }>Lazy 1</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={ ({ isActive }) => activeLink(isActive) }>About</NavLink>
+                            <NavLink to="/lazy2" className={ ({ isActive }) => activeLink(isActive) }>Lazy 2</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={ ({ isActive }) => activeLink(isActive) }>Users</NavLink>
+                            <NavLink to="/lazy3" className={ ({ isActive }) => activeLink(isActive) }>Lazy 3</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="/about" element={ <h1>About Page</h1> }/>
-                    <Route path="/users" element={ <h1>Users Page</h1> }/>
-                    <Route path="/home" element={ <h1>Home Page</h1> }/>
+                    <Route path="/lazy1" element={ <LazyPage1 /> }/>
+                    <Route path="/lazy2" element={ <LazyPage2 /> }/>
+                    <Route path="/lazy3" element={ <LazyPage3 /> }/>
 
                     <Route path="/*" element={ <Navigate to="/home" replace/> }/>
                 </Routes>
